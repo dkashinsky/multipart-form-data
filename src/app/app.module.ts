5a84955f822fdb0b30eed07f59408fa6aa6e4b6e
Dmitry Kashinsky <dmitry.kashinsky@gmail.com>
2019-05-08 00:08:47 +0400

    initial components set for test purpose

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MultipartFormComponent } from './multipart-form/multipart-form.component';
+import { FileInputComponent } from './file-input/file-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultipartFormComponent,
+    FileInputComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/multipart-form/multipart-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multipart-form',
+  templateUrl: './multipart-form.component.html',
+  styleUrls: ['./multipart-form.component.scss']
+})
+export class MultipartFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
